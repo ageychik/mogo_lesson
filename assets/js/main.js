0f613b5084a4addcca106591437d4c7b881cca67
Roma <forwm.ru@gmail.com>
2019-04-15 22:10:14 +0300

    created header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
-$( function() {
-    $( ".second-service-accordion" ).accordion();
-} );
+$(function() {
+
+    // Инициализация
+    (function init(){
+        $( ".second-service-accordion" ).accordion();
+        header();
+        event();
+    })();
+
+
+    // Обработчики
+    function event(){
+        $(window).on('scroll', header);
+    }
+
+    // Логика
+    function header(){
+        var header = $('.section-header');
+        header.toggleClass('_bg', $('.section-about-us').offset().top - header.outerHeight() <= $(window).scrollTop());
+        header.toggleClass('fixed', $(window).scrollTop() > 0);
+    }
+});
